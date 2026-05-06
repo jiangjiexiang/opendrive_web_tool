4a8a8da3f5122ef09a7cbec845ee8c3cbb387ab6
JieXiang <jiangjiexiang70@gmail.com>
2026-05-06 10:16:18 +0800

    增加多分辨率适配

--- native/build/_deps/pugixml-build/CTestTestfile.cmake
+++
@@ -1,6 +0,0 @@
-# CMake generated Testfile for 
-# Source directory: /home/jiang/workspace/opendrive_web_tool/native/build/_deps/pugixml-src
-# Build directory: /home/jiang/workspace/opendrive_web_tool/native/build/_deps/pugixml-build
-# 
-# This file includes the relevant testing commands required for 
-# testing this directory and lists subdirectories to be tested as well.
